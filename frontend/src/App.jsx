d4cce39da4fb7f9904d616a9a2eb098bfc79831c
Tyler Tapia <ttapia13@wgu.edu>
2026-08-22 05:19:46 -1000

    Initialize Spring Boot backend and React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+      <main className="app">
+        <h1>Japan Travel Planner</h1>
+        <p>Plan and manage your trip to Japan</p>
+      </main>
+  )
+}
+
+export default App
